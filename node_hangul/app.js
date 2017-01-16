7bd4395355bfd682e0cbb1d159933aa48013cd7e
ryuken73 <ryuken73@gmail.com>
2017-01-17 07:31:53 +0900

    add

--- node_hangul/app.js
+++ node_hangul/app.js
@@ -9,7 +9,7 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+// view engine setup1
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
