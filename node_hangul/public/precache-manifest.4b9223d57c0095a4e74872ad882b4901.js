e82e0bd24d354c839cd023c1cb7250c53ddf86d7
ryuken <ryuken73@gmail.com>
2020-02-17 19:08:06 +0900

    react monitor builds

---
+++ node_hangul/public/precache-manifest.4b9223d57c0095a4e74872ad882b4901.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4442fe526cc6ad94f1918fca53f5c3ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a01bfaa338191f9456f",
+    "url": "/static/css/main.fcc7dbbf.chunk.css"
+  },
+  {
+    "revision": "883306266f93f39a668f",
+    "url": "/static/js/2.ee138201.chunk.js"
+  },
+  {
+    "revision": "2549765df2eb04fa59a1f28130514f65",
+    "url": "/static/js/2.ee138201.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a01bfaa338191f9456f",
+    "url": "/static/js/main.5aacfd92.chunk.js"
+  },
+  {
+    "revision": "713fa1ea2311902e3d3b",
+    "url": "/static/js/runtime-main.415a732f.js"
+  }
+]);
