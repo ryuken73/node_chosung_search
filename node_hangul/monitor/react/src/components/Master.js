44b3cd7ccee78e567f397fb12cb1ffa2c71a35b3
ryuken <ryuken73@gmail.com>
2020-02-23 18:41:59 +0900

    change right/left pannel to master/worker js

--- node_hangul/monitor/react/src/components/Master.js
+++ node_hangul/monitor/react/src/components/Master.js
@@ -6,23 +6,12 @@ import {makeStyles} from '@material-ui/core/styles';
 import Tooltip from '@material-ui/core/Tooltip';
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-    backgroundColor: theme.palette.background.paper,
-  },
-  gridList: {
-    width: '100%',  
-    height: '100%',
-  },
   icon: {
     color: 'rgba(255, 255, 255, 0.54)',
   },
 }));
 
-export default function RightPanel({master, currentLog}) {
+export default function Master({master, currentLog}) {
   // console.log('render master')
   const classes = useStyles();
   const lastIndexedCount = master.lastIndexedCount ? master.lastIndexedCount : 0;
@@ -30,8 +19,8 @@ export default function RightPanel({master, currentLog}) {
   const logStyle = {display:'flex', flexGrow:0, flexShrink:1, flexBasis:"130px", overflow:'hidden'};
   return (
     <Box  display="flex" flexDirection="column" justifyContent="flex-start" alignItems="center" flexGrow={1} flexBasis={0} bgcolor={brown[800]}>
-        <Box component="div">master</Box>
-        <Box display="flex" width="100%" alignItems="center" justifyContent="space-around" flexDirection="row" height="20vh" m={1}  fontSize="fontSize" bgcolor={brown[700]}>
+        <Box >master</Box>
+        <Box display="flex" width="95%" alignItems="center" justifyContent="space-around" flexDirection="row" height="20vh" m={1}  fontSize="fontSize" bgcolor={brown[700]}>
           <Box display="flex" justifyContent="flex-start" flexDirection="column" m={2} textAlign="left">
             <div>PID: {master.pid}</div> 
             <div>MEM: {master.mem}</div>
@@ -45,7 +34,7 @@ export default function RightPanel({master, currentLog}) {
              <div style={{fontSize:"60px"}}>{searching}</div> 
           </Box> 
         </Box>
-        <Box component="div" overflow="auto" textOverflow="ellipsis" width="100%" height="100vh" fontSize="fontSize" bgcolor={brown[700]}>
+        <Box component="div" overflow="auto" textOverflow="ellipsis" width="95%" height="50vh" fontSize="12px" bgcolor={brown[700]}>
           {currentLog.map(log => (
             <div style={{display:"flex", marginLeft:"10px", marginTop:'3px', flexDirection:"row", justifyContent:"flex-start", height:"1.2em", color: log.cacheHit && 'cyan'}}>
               <div style={logStyle}>{log.eventTime}</div>
