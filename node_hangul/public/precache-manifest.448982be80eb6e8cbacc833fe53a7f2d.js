8a76d6afaff805985cdee4db0343a45b59126b3c
ryuken73 <ryuken73@gmail.com>
2020-06-09 20:21:44 +0900

    etc: build monitor

---
+++ node_hangul/public/precache-manifest.448982be80eb6e8cbacc833fe53a7f2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ec4333231701d58d61efa42bc4ad40b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "851fe0fa459a331732a7",
+    "url": "/static/css/main.76cd3051.chunk.css"
+  },
+  {
+    "revision": "98294eb5ae947583206d",
+    "url": "/static/js/2.5e92c94d.chunk.js"
+  },
+  {
+    "revision": "a51e5369284a8e3f2f9317fd6575586b",
+    "url": "/static/js/2.5e92c94d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "851fe0fa459a331732a7",
+    "url": "/static/js/main.623b0d6e.chunk.js"
+  },
+  {
+    "revision": "713fa1ea2311902e3d3b",
+    "url": "/static/js/runtime-main.415a732f.js"
+  }
+]);
