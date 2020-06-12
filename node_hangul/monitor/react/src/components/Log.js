6db621e863e2d1f4f26b115cdc1d5df268d5530a
ryuken <ryuken73@gmail.com>
2020-06-12 22:45:06 +0900

    bug fix : MUI Tooltip works (Box doesn't works as child of Tooltip)

--- node_hangul/monitor/react/src/components/Log.js
+++ node_hangul/monitor/react/src/components/Log.js
@@ -6,20 +6,21 @@ import Tooltip from '@material-ui/core/Tooltip';
 function Log({gap, currentLog}) {
   console.log('rerendering Log : ', currentLog);
   const logStyle = {display:'flex', flexGrow:0, flexShrink:0, flexBasis:"130px", overflow:'hidden'};
-  const ConditionalTooltip = ({keyword, children}) => {
-    if(keyword.length > 50){
+  const ConditionalTooltip = props => {
+    const {keyword, children} = props;
+    if(keyword.length > 20){
       return <Tooltip title={keyword}>{children}</Tooltip>
     } else {
       return <div>{children}</div>
     }
   }
-  const ColorBox = (props) => {
+  const ColorBox = props => {
     const {cacheHit, children} = props;
-    return <Box color={cacheHit && 'cyan'}>{children}</Box>
+    return <div style={{"color": cacheHit && "cyan"}}>{children}</div>
   }
   const defaultStyle = {flexGrow:1, flexShrink:5, margin:'3px', minWidth:'120px', flexBasis:'120px'};
   return (
-    <Box display="flex" flexDirection="row" justifyContent="flex-start" alignItems="stretch" flexGrow={3} mx={gap} overflow="auto" overflowX="hidden" textOverflow="ellipsis" fontSize="12px" bgcolor={brown[700]}>
+    <Box display="flex" flexDirection="row" justifyContent="flex-start" alignItems="stretch" flexGrow={3} mx={gap} overflow="auto" textOverflow="ellipsis" fontSize="12px" bgcolor={brown[700]} style={{"overflowX":"hidden"}}>
       <Box style={{...defaultStyle, minWidth:'120px'}}>
         {currentLog.map(log => <ColorBox cacheHit={log.cacheHit}>{log.eventTime}</ColorBox>)}
       </Box>
@@ -36,21 +37,14 @@ function Log({gap, currentLog}) {
         {currentLog.map(log => <ColorBox cacheHit={log.cacheHit}>{log.resultCount}건</ColorBox>)}
       </Box>
       <Box style={{...defaultStyle, flexGrow:5, flexShrink:1, whiteSpace:'nowrap', textOverflow:'ellipsis'}}>
-        {currentLog.map(log => <ColorBox cacheHit={log.cacheHit}>{log.keyword}</ColorBox>)}
-      </Box>
-      {/* {currentLog.map(log => (
-        <div style={{display:"flex", marginLeft:"10px", marginTop:'3px', flexDirection:"row", justifyContent:"flex-start", height:"1.2em", color: log.cacheHit && 'cyan'}}>
-          <div style={{minWidth:'120px',...logStyle}}>{log.eventTime}</div>
-          <div style={{minWidth:'60px',...logStyle}}>{log.userId}</div>
-          <div style={{minWidth:'80px',...logStyle}}>{log.ip}</div>
-          <div style={{minWidth:'60px',...logStyle}}>{log.elapsed}초</div>
-          <div style={{minWidth:'60px',...logStyle} }>{log.resultCount}건</div>
+        {currentLog.map(log => 
+          (
           <ConditionalTooltip keyword={log.keyword}>
-            <div style={{display:'flex', flexShrink:0, flexBasis:"auto", minWidth:'200px', maxWidth:'300px', overflow:'hidden', textOverflow:'ellipsis'}}>{log.keyword}</div>
+            <ColorBox cacheHit={log.cacheHit}>{log.keyword}</ColorBox>
           </ConditionalTooltip>
-
-        </div>
-      ))} */}
+          )
+        )}
+      </Box>
     </Box>
   )
 }
