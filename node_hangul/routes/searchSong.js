51b60288deba9b59dffc78f43bc46ff2a390ed06
ryuken73 <ryuken73@gmail.com>
2020-06-24 23:39:11 +0900

    refactor : remove unused code in searchSong.js

--- node_hangul/routes/searchSong.js
+++ node_hangul/routes/searchSong.js
@@ -93,51 +93,6 @@ router.get('/withWorkers/:pattern', async (req, res, next) => {
 }); 
 
 
-// function artistNameStartsFirst(patternOriginal){
-// 	return (a, b) => {
-// 		const pattern = patternOriginal.replace(/\s+$/,'');
-// 		const AB = -1;
-// 		const BA = 1;
-// 		// one of two records' artistName start with exact pattern, move forward
-// 		if(a.artistName.startsWith(pattern) && !b.artistName.startsWith(pattern)) return AB;
-// 		if(b.artistName.startsWith(pattern) && !a.artistName.startsWith(pattern)) return BA;
-// 		return 0;
-// 	}
-// }
-
-// function artistNameIncludesFirst(patternOriginal){
-// 	return (a, b) => {
-// 		const pattern = patternOriginal.replace(/\s+$/,'');
-// 		const AB = -1;
-// 		const BA = 1;
-// 		// one of two records' artistName start with exact pattern, move forward
-// 		if(a.artistName.includes(pattern) && !b.artistName.includes(pattern)) return AB;
-// 		if(b.artistName.includes(pattern) && !a.artistName.includes(pattern)) return BA;
-// 		return 0;
-// 	}
-// }
-
-// function orderyByKey(patternOriginal){
-// 	return (a, b) => {
-// 		const sortByKey = sortBy(a,b);
-// 		const sortByArtistName = sortByKey('artistName');
-// 		const sortBysongName = sortByKey('songName');
-// 		return sortByArtistName || sortBysongName || 0;
-// 	}
-// }
-
-// function sortBy(a,b) {
-// 	return key => {
-// 		// if key value is empty, push back
-// 		if(a[key] === '') return 1;
-// 		if(b[key] === '') return -1;
-// 		// normal order
-// 		if(a[key] > b[key]) return 1;
-// 		if(a[key] < b[key]) return -1;
-// 		return false
-// 	}
-// }
-
 async function lookupCache({cacheWorkers, patternJAMO, userFrom}){
 	const {ip, userId} = userFrom;
 	const cacheSearchJob = {
@@ -206,27 +161,6 @@ function broadcastSearch(masterMonitorStore, type){
 	type === 'end' && masterMonitorStore.setStatus('searching', searchMonitorAfterSearch-1);
 	masterMonitorStore.broadcast({eventName:'masterMonitor'});	
 }
-
-// function sortThreeWords(patternOriginal){
-// 	return (a, b) => {
-// 		const pattern = patternOriginal.replace(/\s+$/,'');
-// 		const AB = -1;
-// 		const BA = 1;
-// 		// one of two records' artistName start with exact pattern, move forward
-// 		if(a.artistName.startsWith(pattern) && !b.artistName.startsWith(pattern)) return AB;
-// 		if(b.artistName.startsWith(pattern) && !a.artistName.startsWith(pattern)) return BA;
-// 		// if two records starts or not starts with exact pattern,
-// 		// one of two records' artistName includes exact pattern, move forward
-// 		if(a.artistName.includes(pattern) && !b.artistName.includes(pattern)) return AB;
-// 		if(b.artistName.includes(pattern) && !a.artistName.includes(pattern)) return BA;
-// 		// if not starts with and not include patterns, (if patterns has multiple words and partially matched)
-// 		// just order by artistName and then songName
-// 		const sortByKey = sortBy(a,b);
-// 		const sortByArtistName = sortByKey('artistName');
-// 		const sortBysongName = sortByKey('songName');
-// 		return sortByArtistName || sortBysongName || 0;
-// 	}
-// }
 
 function sortMultiFields(a, b){
 	if(a.weight > b.weight) return 1;
