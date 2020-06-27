55e03a5a1ab41123106d3a2fd18bbb34a1e44b94
ryuken73 <ryuken73@gmail.com>
2020-06-27 17:07:26 +0900

    refactor : remove all other then three words search

---
+++ node_hangul/lib/songClass.js
@@ -0,0 +1,46 @@
+const hangul = require('hangul-js');
+
+class Song {
+    constructor([artistName='', songName='']){
+        this._artistName = this.clearWord(artistName);
+        this._songName = this.clearWord(songName);   
+        this._combinedName = this.mkCombinedName();
+        this._jamoCombinedName = this.getJAMO(this._combinedName);
+        return this;     
+    }    
+    getJAMO = (hangulStr) => {
+        return hangul.disassemble(hangulStr).join('');	
+    }
+    clearWord = (word) => {
+        return word.replace(/\s+/g, " ").trim().replace(/^"/gi, '').replace(/"$/gi, '').replace(/\s+$/gi, '');
+    }
+    mkCombinedName = () => {
+        const artistNsongNartist = `${this._artistName} ${this._songName} ${this._artistName}`;
+        const artistNsongNartistNoBlank = `${this._songName.replace(/\s+/g, '')}${this._artistName.replace(/\s+/g, '')}${this._songName.replace(/\s+/g, '')}`;
+        return `${artistNsongNartist} ${artistNsongNartistNoBlank}`
+    }
+    get artistName(){
+        return this._artistName;
+    }
+    get songName(){
+        return this._songName;
+    }
+    get combinedName(){
+        return this._combinedName;
+    }
+    get jamoArtist(){
+        return this.getJAMO(this._artistName);
+    }
+    get jamoSong(){
+        return this.getJAMO(this._songName);
+    }
+    get jamoCombinedName(){
+        return this._jamoCombinedName;
+    }
+}
+
+module.exports = {
+    create([artistName='', songName='']){
+        return new Song([artistName, songName]);
+    }
+}
---
+++ node_hangul/lib/workerMgr.js
@@ -0,0 +1,76 @@
+const getMemInfo = require('./getMemInfo');
+const hangul = require('hangul-js');
+const orderSong = require('./orderSong');
+
+
+const worker = {
+    init : () => {
+        this.pid = process.pid;
+        this.cache = new Map();
+        this.cacheCount = 0;
+        this.cacheHit = 0;
+        return this;
+    },
+    lookup : (pattern) => {
+        const result = this.cache.get(pattern) || [];
+        result.length > 0 && this.cacheHit++;
+        return result;
+    },
+    update : (pattern, results) => {
+        // console.log(`set cache[${pattern}]:`, results)
+        this.cache.set(pattern, results);
+        this.cacheCount++;
+        // console.log([...this.cache])
+        return true;
+    },
+    delete : (pattern) => {
+        this.cache.delete(pattern);
+        return true;
+    },
+    clear : () => {
+        this.cache = new Map();
+        this.cacheCount = 0;
+        return true;
+    }
+}
+
+process.on('message', ({reqId, job}) => {
+    const {cmd, pattern, results=[]} = job;
+    let result;
+    switch(cmd){
+        // case 'init' :
+        //     result = cache.create();
+        //     break;
+        case 'get' :
+            result = cache.lookup(pattern);
+            break;
+        case 'put' :
+            result = cache.update(pattern, results);
+            break;
+        case 'delete' :
+            result = cache.delete(pattern, results);
+            break;
+        case 'clear' :
+            result = cache.clear();
+            break;
+        case 'requestMonitor' :
+            const {pid, cacheCount, cacheHit} = this;
+            result = {
+                pid,
+                cacheCount,
+                cacheHit,
+                mem: getMemInfo()
+            }
+            // console.log(result)
+            break;
+    }
+    // console.log(`cache work done: ${process.pid}: cmd = ${cmd}`);
+    process.send({
+        resId : reqId,
+        success: true,
+        result
+    })
+})
+
+// initialize cache
+worker.init();
