b2024dd33bcbf07e0240f461cd4965a5d94a0a59
ryuken73 <ryuken73@gmail.com>
2020-06-29 00:27:33 +0900

    enhance : request in childProcManager can deal both all workers and worker request

--- node_hangul/lab/childProcManager.js
+++ node_hangul/lab/childProcManager.js
@@ -2,47 +2,6 @@
 const child_process = require('child_process');
 const debug = require('debug')('childProcManager');
 
-const forkWorkers = (jsFile, args, count) => {
-    const array = new Array(count);
-    array.fill(1);    
-    return array.map( element => child_process.fork(jsFile, args));
-}
-
-const defaultExitCallback = (oldWorker, newWorker) => {
-
-}
-
-const _initGetNextIdAPI = worker => {
-    worker.nextRequestId = () => {
-        const pid = worker.pid;
-        worker.jobId = worker.jobId === undefined ? 1 : ++worker.jobId;
-        return `${pid}_${worker.jobId}`;
-    }
-}
-
-const _initRequestAPI = worker => {
-    worker.promise = {};
-    worker.promise.request = request => {
-        return new Promise((resolve, reject) => {
-            const requestId = worker.nextRequestId();
-            worker.send({requestId, request});
-            const handleResponse = response => {
-                const {responseId, success, result} = response;
-                if(responseId === requestId) {
-                    console.log(`[workerPool][${worker.pid}]got responseId =`, responseId, success)
-                    worker.removeListener('message', handleResponse);
-                    if(success) {
-                        resolve(result);
-                    } else {
-                        reject(worker.pid);
-                    }
-                }            
-            }  
-            worker.on('message', handleResponse);  
-        })
-    }
-}
-
 class ChildProcessManager {
     constructor(jsFile, args=[], count=2, customExitCallback=defaultExitCallback){
         this.jsFile = jsFile;
@@ -55,19 +14,22 @@ class ChildProcessManager {
 
     get workers(){ return this._workers}
 
-    request(request){
+    request(request, worker){
+        if(worker && worker.promise){
+            return worker.promise.request(request);
+        }
         const requests = this.workers.map(worker => worker.promise.request(request));
         return Pomise.all(requests);
     }
 
     _initWorker(worker){
-        _initGetNextIdAPI(worker);
-        _initRequestAPI(worker);
-        this._initErrorHandler(worker);
-        this._initExitHandler(worker);
+        _attachGetNextIdAPI(worker);
+        _attachRequestAPI(worker);
+        this._attachErrorHandler(worker);
+        this._attachExitHandler(worker);
     }
 
-    _initExitHandler(worker){
+    _attachExitHandler(worker){
         worker.on('exit', (code,signal) => {
             console.log(`*********** worker exit : [${worker.pid}][${code}][${signal}]`);
             const oldWorker = worker;
@@ -77,9 +39,54 @@ class ChildProcessManager {
         })
     }
 
-    _initErrorHandler(worker){
+    _attachErrorHandler(worker){
         worker.on('error', (err) => {
             console.error(`*********** worker error : [${worker.pid}]`, err);
+        })
+    }
+}
+
+const forkWorkers = (jsFile, args, count) => {
+    const array = new Array(count);
+    array.fill(1);    
+    return array.map( element => child_process.fork(jsFile, args));
+}
+
+const defaultExitCallback = (oldWorker, newWorker) => {
+
+}
+
+const _attachGetNextIdAPI = worker => {
+    worker.nextRequestId = () => {
+        const pid = worker.pid;
+        worker.jobId = worker.jobId === undefined ? 1 : ++worker.jobId;
+        return `${pid}_${worker.jobId}`;
+    }
+}
+
+const _attachRequestAPI = worker => {
+    worker.promise = {};
+    worker.promise.request = request => {
+        return new Promise((resolve, reject) => {
+            const requestId = worker.nextRequestId();
+            worker.send({requestId, request});
+            const handleResponse = response => {
+                const {responseId, success, result} = response;
+                if(responseId === undefined){
+                    console.error("child process should return response id to manager. or event listenr of workers will increase forever!", response);
+                    process.exit();
+                }
+                if(responseId === requestId) {
+                    // console.log(`[workerPool][${worker.pid}]got responseId =`, responseId, success)
+                    worker.removeListener('message', handleResponse);
+                    if(success) {
+                        resolve(result);
+                    } else {
+                        reject(worker.pid);
+                    }
+                }            
+            }  
+            worker.on('message', handleResponse);  
         })
     }
 }
