3872bb3984451b579f772599e3e36de6ce58beb1
ryuken73 <ryuken73@gmail.com>
2020-07-09 22:56:00 +0900

    bug fix : fix typo

--- node_hangul/routes/loadSong.js
+++ node_hangul/routes/loadSong.js
@@ -32,7 +32,7 @@ router.get('/useWorkers', async (req, res, next) => {
 	}
 	global.logger.info('request accepted');
 	res.send({result:'success', msg:'request accepted'});
-	const totalLoaded = workers ? await masterEngine.loadFromFile(masterMonitor, options) : {};
+	const totalLoaded = masterEngine ? await masterEngine.loadFromFile(masterMonitor, options) : {};
 	const result = totalLoaded ? {result:'success', count: totalLoaded} 
 							   : {result:'failure', count: 0};
 	global.logger.info('total loaded:',result.count);
