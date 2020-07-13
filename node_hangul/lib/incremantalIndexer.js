558c06b7682a6df069dadbd88090a99d51124beb
ryuken73 <ryuken73@gmail.com>
2020-07-13 20:18:52 +0900

    refactor : move incremental indexing hancler to separate file

---
+++ node_hangul/lib/incremantalIndexer.js
@@ -0,0 +1,82 @@
+const masterEngine = require("./masterEngine");
+
+module. exports = (masterEngine, db) => {
+    const deleteIndex = async KEY => {
+        return await masterEngine.searchManager.request({cmd: 'deleteByKey', payload: {key: KEY}});
+    }
+    const deleteCache = async KEY => {
+        const sqlGetDetail = `${global.INDEX_DATA_SQL} where key = ?`;
+        const recordDetail = await db.query(sqlGetDetail, [KEY]);
+        const {ARTIST, SONG_NAME} = recordDetail.shift();
+        const deleteJob = {
+            cmd: 'deleteByValue', 
+            payload: {
+                artistName: ARTIST,
+                songName: SONG_NAME
+            }
+        }
+        return await masterEngine.cacheManager.request(deleteJob);
+    }
+    const addIndex = async KEY => {
+        const sqlGetDetail = `${global.INDEX_DATA_SQL} where key = ?`;
+        const recordDetail = await db.query(sqlGetDetail, [KEY]);
+        const {ARTIST, SONG_NAME, OPEN_DT, STATUS} = recordDetail.shift();
+        const addIndexJob = {
+            cmd : 'index',
+            payload : {
+                data : [ARTIST, SONG_NAME, KEY, OPEN_DT, STATUS]
+            }
+        }
+        const result = await masterEngine.searchManager.nextWorker.promise.request(addIndexJob);
+        global.logger.info(`addIndex : result[${result}] artist[${ARTIST}] song[${SONG_NAME}] status[${STATUS}] key[${KEY}]`);
+        return result;
+    }
+
+    const deleteDBRecord = async record => {
+        const {EVENT_TIME, KEY} = record;
+        const sqlDeleteRecord = `delete from music.ac_search_log where EVENT_TIME = ? and KEY = ?`;
+        const sqlDeleteArgs = [EVENT_TIME, KEY];
+        //const result = await db.execute(sqlDeleteRecord, sqlDeleteArgs);
+        const result = await Promise.resolve(true)
+        global.logger.info(`deleteDBRecord : result[${result}] EVENT_TIME[${EVENT_TIME}] KEY[${KEY}]`);
+    }
+
+    const handleUpdate = async record => {
+        const {EVENT_TIME, KEY} = record
+        const deleteResults = await deleteIndex(KEY);
+        if(deleteResults.some(result => result !== true)){
+            global.logger.error(`delete index failed : `, KEY);
+            return false
+        }
+        const deleteCacheResults = await deleteCache(KEY);
+        if(deleteCacheResults.some(result => result !== true)){
+            global.logger.error(`delete cache failed : `, KEY);
+            return false
+        }
+        const addIndexResults = await addIndex(KEY);
+        if(addIndexResults !== true){
+            global.logger.error('add index failed : ', KEY);
+            return false
+        }
+        return true;
+    }
+
+    const handleInsert = async record => {
+        console.log('handleInsert');
+        return true;
+    }
+
+    const handleDelete = async record => {
+        console.log('handleDelete');
+        return true;
+    }
+
+    return {
+        handleUpdate,
+        handleInsert,
+        handleDelete,
+        deleteDBRecord
+    }
+}
+
+
--- node_hangul/lib/schedulerEngine.js
+++ node_hangul/lib/schedulerEngine.js
@@ -1,9 +1,9 @@
 const cron = require('node-cron');
 
-
 module.exports = (masterEngine, db) => {
 
     const registeredTasks = new Map();
+
     const register = (scheduleString, scheduleName) => {
         const valid = cron.validate(scheduleString);
         if(!valid){
@@ -19,82 +19,10 @@ module.exports = (masterEngine, db) => {
         return true; 
     }
 
-    const deleteIndex = async KEY => {
-        return await masterEngine.searchManager.request({cmd: 'deleteByKey', payload: {key: KEY}});
-    }
-    const deleteCache = async KEY => {
-        const sqlGetDetail = `${global.INDEX_DATA_SQL} where key = ?`;
-        const recordDetail = await db.query(sqlGetDetail, [KEY]);
-        const {ARTIST, SONG_NAME} = recordDetail.shift();
-        const deleteJob = {
-            cmd: 'deleteByValue', 
-            payload: {
-                artistName: ARTIST,
-                songName: SONG_NAME
-            }
-        }
-        return await masterEngine.cacheManager.request(deleteJob);
-        // return await masterEngine.cacheManager.request({cmd: 'deleteByKey', payload: {key: KEY}});
-    }
-    const addIndex = async KEY => {
-        const sqlGetDetail = `${global.INDEX_DATA_SQL} where key = ?`;
-        const recordDetail = await db.query(sqlGetDetail, [KEY]);
-        const {ARTIST, SONG_NAME, OPEN_DT, STATUS} = recordDetail.shift();
-        const addIndexJob = {
-            cmd : 'index',
-            payload : {
-                data : [ARTIST, SONG_NAME, KEY, OPEN_DT, STATUS]
-            }
-        }
-        const result = await masterEngine.searchManager.nextWorker.promise.request(addIndexJob);
-        global.logger.info(`addIndex : result[${result}] artist[${ARTIST}] song[${SONG_NAME}] status[${STATUS}] key[${KEY}]`);
-        return result;
-    }
-
-    const deleteDBRecord = async record => {
-        const {EVENT_TIME, KEY} = record;
-        const sqlDeleteRecord = `delete from music.ac_search_log where EVENT_TIME = ? and KEY = ?`;
-        const sqlDeleteArgs = [EVENT_TIME, KEY];
-        //const result = await db.execute(sqlDeleteRecord, sqlDeleteArgs);
-        const result = await Promise.resolve(true)
-        global.logger.info(`deleteDBRecord : result[${result}] EVENT_TIME[${EVENT_TIME}] KEY[${KEY}]`);
-    }
-
-    const handleUpdate = async record => {
-        const {EVENT_TIME, KEY} = record
-        const deleteResults = await deleteIndex(KEY);
-        if(deleteResults.some(result => result !== true)){
-            global.logger.error(`delete index failed : `, KEY);
-            return false
-        }
-        const deleteCacheResults = await deleteCache(KEY);
-        if(deleteCacheResults.some(result => result !== true)){
-            global.logger.error(`delete cache failed : `, KEY);
-            return false
-        }
-        const addIndexResults = await addIndex(KEY);
-        if(addIndexResults !== true){
-            global.logger.error('add index failed : ', KEY);
-            return false
-        }
-        return true;
-    }
-
-    const handleInsert = async record => {
-        console.log('handleInsert');
-        return true;
-        // console.log(record)
-    }
-
-    const handleDelete = async record => {
-        console.log('handleDelete');
-        return true;
-        // console.log(record);
-    }
-
-
+    const incrIndexer = require('./incremantalIndexer')(masterEngine, db);
     const schedulableTasks = {
         'incremental_v_song' : async () => {
+            const {handleUpdate, handleInsert, handleDelete, deleteDBRecord} = incrIndexer;
             global.logger.info('schedule tiggerred');
             const sqlGetChanged = 'select * from music.ac_search_log order by event_time asc';
             const changedRecords = await db.query(sqlGetChanged, []);
