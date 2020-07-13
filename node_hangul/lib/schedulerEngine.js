9e67006f85a7b456db3d49f1ed551ea7568a0875
ryuken <ryuken73@gmail.com>
2020-07-13 18:04:40 +0900

    etc : remove unused code and add final action (deleteDBRecord)

--- node_hangul/lib/schedulerEngine.js
+++ node_hangul/lib/schedulerEngine.js
@@ -47,16 +47,17 @@ module.exports = (masterEngine, db) => {
             }
         }
         const result = await masterEngine.searchManager.nextWorker.promise.request(addIndexJob);
-        global.logger.info(`addIndex : result[${result}] artistName[${ARTIST}] songName[${SONG_NAME}] status[${STATUS}] open[${OPEN_DT}] key[${KEY}]`);
+        global.logger.info(`addIndex : result[${result}] artist[${ARTIST}] song[${SONG_NAME}] status[${STATUS}] key[${KEY}]`);
         return result;
     }
 
-    const deleteDBRecord = async (EVENT_TIME, KEY) => {
+    const deleteDBRecord = async record => {
+        const {EVENT_TIME, KEY} = record;
         const sqlDeleteRecord = `delete from music.ac_search_log where EVENT_TIME = ? and KEY = ?`;
         const sqlDeleteArgs = [EVENT_TIME, KEY];
-        const result = await db.execute(sqlDeleteRecord, sqlDeleteArgs);
+        //const result = await db.execute(sqlDeleteRecord, sqlDeleteArgs);
+        const result = await Promise.resolve(true)
         global.logger.info(`deleteDBRecord : result[${result}] EVENT_TIME[${EVENT_TIME}] KEY[${KEY}]`);
-        console.log(result);
     }
 
     const handleUpdate = async record => {
@@ -76,13 +77,18 @@ module.exports = (masterEngine, db) => {
             global.logger.error('add index failed : ', KEY);
             return false
         }
+        return true;
     }
 
     const handleInsert = async record => {
+        console.log('handleInsert');
+        return true;
         // console.log(record)
     }
 
     const handleDelete = async record => {
+        console.log('handleDelete');
+        return true;
         // console.log(record);
     }
 
@@ -93,34 +99,11 @@ module.exports = (masterEngine, db) => {
             const sqlGetChanged = 'select * from music.ac_search_log order by event_time asc';
             const changedRecords = await db.query(sqlGetChanged, []);
             changedRecords.forEach(async record => {
-                record.IUD_TYPE === 'U' && handleUpdate(record) 
-                record.IUD_TYPE === 'I' && handleInsert(record)
-                record.IUD_TYPE === 'D' && handleDelete(record) 
+                const result = record.IUD_TYPE === 'U' && await handleUpdate(record) || 
+                               record.IUD_TYPE === 'I' && await handleInsert(record) ||
+                               record.IUD_TYPE === 'D' && await handleDelete(record); 
+                if(result === true) await deleteDBRecord(record);
             })
-
-
-            // const updateChanges = changedRecords.filter(record => record.IUD_TYPE === 'U');
-            // const insertChanges = changedRecords.filter(record => record.IUD_TYPE === 'I');
-            // const deleteChanges = changedRecords.filter(record => record.IUD_TYPE === 'D');
-            // global.logger.info('result of schedule[updateChange] : ', updateChanges);
-            // global.logger.info('result of schedule[insertChange] : ', insertChanges);
-            // global.logger.info('result of schedule[deleteChange] : ', deleteChanges);
-            // updateChanges.length > 0 && handleUpdate(updateChanges) && deleteRecords(updateChanges);
-            // insertChanges.length > 0 && handleInsert(insertChanges) && deleteRecords(insertChanges);
-            // deleteChanges.length > 0 && handleDelete(deleteChanges) && deleteRecords(deleteChanges);
-
-
-
-            // changedRecords.forEach(async record => {
-            //     const {EVENT_TIME, KEY, IDU_TYPE} = record;
-            //     const sqlGetDetail = `${global.INDEX_DATA_SQL} where key = ?`;
-                // const recordDetail = await db.query(sqlGetDetail, [KEY]);
-                // console.log(recordDetail.shift());
-                // const {ARTIST, SONG_NAME, OPEN_DT, STATUS} = recordDetail.unshift();
-                // const wordArray = [ARTIST, SONG_NAME, KEY, OPEN_DT, STATUS];
-                // global.logger.info(wordArray);
-
-            // }) 
         } 
     }
     
