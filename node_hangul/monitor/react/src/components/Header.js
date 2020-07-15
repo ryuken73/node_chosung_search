78daef40b643413b417db7f6b48415eb8b5110df
ryuken <ryuken73@gmail.com>
2020-07-15 17:11:06 +0900

    etc : change UI of dynamic indexing status(UI)

--- node_hangul/monitor/react/src/components/Header.js
+++ node_hangul/monitor/react/src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Box from '@material-ui/core/Box';
 import {brown} from '@material-ui/core/colors';
+import Chip from '@material-ui/core/Chip';
 
 export default function Header({gap, text, insert=0, update=0, deleteCount=0}) {
     return (
@@ -9,10 +10,10 @@ export default function Header({gap, text, insert=0, update=0, deleteCount=0}) {
           <Box width="1" textAlign="center" mx="auto" >
             {text}
           </Box>
-          <Box display="flex" width="10%" flexDirection="column" ml="auto" minWidth="100px" mr="10px">
-            <Box fontSize="caption.fontSize">I : {insert}</Box>
-            <Box fontSize="caption.fontSize">U : {update}</Box>
-            <Box fontSize="caption.fontSize">D : {deleteCount}</Box>
+          <Box bgcolor={brown[700]} display="flex" width="10%" justifyContent="center" flexDirection="column" ml="auto" minWidth="100px" mr="10px">
+            <Box mt="4px" ml="10px" color="white" fontSize="caption.fontSize">Insert : {insert}</Box>
+            <Box mt="4px" ml="10px" color="white" fontSize="caption.fontSize">Update : {update}</Box>
+            <Box my="4px" ml="10px" color="white" fontSize="caption.fontSize">Delete : {deleteCount}</Box>
           </Box>
         </Box>
       </Box>
